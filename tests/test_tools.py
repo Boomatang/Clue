cc522c6ba9eb48dfbde37aeb1c9b3a6091357ed2
Jim Fitzpatrick <jimfity@gmail.com>
2019-01-01 18:26:44 +0000

    Non Number tests added

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -9,3 +9,22 @@ def test_valid_form_is_entered(client):
                            data=data, follow_redirects=True)
 
     assert b"Actual gap : <strong>96.0</strong>" in response.data
+
+
+def test_non_valid_form_is_entered(client):
+    data = {"post_to_post": "1500",
+            "spacing": "100",
+            "bar_size": "four"}
+    response = client.post(url_for("tools.bar_spacer"),
+                           data=data, follow_redirects=True)
+
+    assert b"input needs to be a number" in response.data
+
+
+def test_non_valid_form_is_entered_for_louver(client):
+    data = {'height': '450',
+            "width": "None"}
+    response = client.post(url_for("tools.louver_calculator"),
+                           data=data, follow_redirects=True)
+
+    assert b"input needs to be a number" in response.data
