a7a9e7134582937fdc254c4baa72b31fa97b4d17
Jim Fitzpatrick <jimfity@gmail.com>
2019-06-01 17:57:07 +0100

    Dependicnes of the plate data has been removed

--- tests/test_bom_parent_qty.py
+++ tests/test_bom_parent_qty.py
@@ -25,6 +25,17 @@ def test_creating_bom(client, setup_for_creating_bom):
     assert expected == result
 
 
+def test_creating_bom_with_no_plate(client, setup_for_creating_bom_with_no_plate):
+    expected = 27
+    bom = CreateBom(setup=1, data=1)
+    bom.run()
+
+    login_standard_user(client)
+    result: BomResult = bom.create_result()
+    result = result.required_length_qty("large", 6500)
+    assert expected == result
+
+
 def test_raw_bom_file(csv_file):
     expected = RawBomFile
     result: RawBomFile = RawBomFile(csv_file)
